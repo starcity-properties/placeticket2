c1d7a6fe5cf727e695545cdbf5fdbb1adceaf3e7
Josh Lehman <jalehman37@gmail.com>
2018-02-24 11:18:30 -0800

    Initial Commit

---
+++ Placeticket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Placeticket
+//
+//  Created by Josh Lehman on 2/24/18.
+//  Copyright © 2018 Starcity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
